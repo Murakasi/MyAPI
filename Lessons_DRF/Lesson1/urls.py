bef57dcbdf6d6497c198eebc872ab501a06d1a99
Ruslan Bihun <bigun.ruslan@gmail.com>
2025-04-21 11:07:41 +0200

    add model Book in models.py

--- Lessons_DRF/Lesson1/urls.py
+++ Lessons_DRF/Lesson1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Book
+from .views import BookList
 urlpatterns = [
-    path('books', Book.as_view(), name='book'),
+    path('books', BookList.as_view(), name='book'),
 ]
--- Lessons_DRF/Lesson1/views.py
+++ Lessons_DRF/Lesson1/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import status, generics
 
 # Create your views here.
+
+class BookList(generics.GenericAPIView):
+    pass
